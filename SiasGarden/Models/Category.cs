81c5f33c411775049f3a98911e95d31827ff1964
karinastrand <karinastrand@yahoo.se>
2024-04-30 17:36:23 +0200

    Category built

--- SiasGarden/Models/Category.cs
+++ SiasGarden/Models/Category.cs
@@ -6,9 +6,14 @@ namespace SiasGarden.Models
     {
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Namn")]
+        [MaxLength(50,ErrorMessage="Maxlängd är 50 tecken")]
         public string Name { get; set; }
-        [Required]
-        public string Description { get; set; }
+        [MaxLength(100, ErrorMessage = "Maxlängd är 100 tecken")]
+        [Display(Name = "Beskrivning")]
+        public string? Description { get; set; }
+        [Display(Name = "Sorteringsordning")]
+        [Range(1,1000,ErrorMessage ="Sorteringsordning måste vara mellan 1 och 1000")]
         public int DisplayOrder { get; set; }
 
     }
