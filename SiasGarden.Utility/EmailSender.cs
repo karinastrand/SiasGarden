c2f21e88423110d357a09399ce739235a622020f
karinastrand <karinastrand@yahoo.se>
2024-05-15 22:49:01 +0200

    .

--- SiasGarden.Utility/EmailSender.cs
+++ SiasGarden.Utility/EmailSender.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 namespace SiasGarden.Utility;
 
 public class EmailSender : IEmailSender
-{
+{//Will add when i have a grid mail account
     public string MailerSendSecret { get; set; }
    
     public Task SendEmailAsync(string email, string subject, string htmlMessage)
